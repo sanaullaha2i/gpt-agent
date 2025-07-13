fc29312200503b91cad7f999187370641f3bf4f1
sanaullaha2i <md.sanaullah@a2i.gov.bd>
2025-07-13 10:07:42 +0000

    updated from ubuntu

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 //console.log(process.env.OPENAI_API_KEY);
 const app = express();
-const PORT = 3000;
+const PORT = 3003;
 
 app.use(cors());
 app.use(express.json());
